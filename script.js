ef420c97699e11202a5d32000f1471c68e10d997
yasharshamilli <yasharesh@code.edu.az>
2024-11-01 18:49:36 +0400

    homework

---
+++ script.js
@@ -0,0 +1,106 @@
+// let price = 45
+// let balance = 55
+// if (balance>=price) { 
+//    balance -= price
+//     console.log("purchase compete "+" remaining funds"+balance);
+// }
+
+// else {
+// notenoughbalance=price-balance
+// console.log("kifayet qeder vesait yoxdur");
+
+// } 
+// let price=130
+// let status="VIP"
+// if (status="VIP") {
+//     price=price* 0.8
+//     console.log(price);
+    
+// }
+// else{
+//     console.log(price);
+    
+// }
+// const num1 = 9;
+// const num2 = 5;
+// const operator = "/" ;
+// if (operator== "+") {
+// console.log(num1+num2);
+// }   
+// else if (operator == "-") {
+//     console.log(num1 - num2); 
+
+// }
+// else if(num2 == 0){
+//     console.log("false");
+    
+
+// }
+// else if (operator == "/") {
+//      console.log(num1 / num2); 
+// }
+// else if (operator == "*") {
+//     console.log(num1 * num2 );
+    
+// }
+// let username = "yashar"
+// let password = 2007
+// let enterusername = " y011"
+// let enterpassword = "7002"
+// if (enterusername=username && password==enterpassword) {
+//     console.log("correct username or password");
+// }
+// else if (enterusername!==enterusername && enterpassword!==enterpassword) {
+//     console.log("correct username or password");  
+// }
+// else if (username!==enterusername ) {
+//     console.log("incorrect username");
+    
+// }
+// else {
+// console.log("incorrect password");
+
+// }
+// let num1=11
+// if (num1 % 2 ==0) {
+//     console.log("bu eded cutdur");
+    
+// }
+// else {
+//     console.log("bu eded tekdir");
+    
+// }
+// let tempreature= 18
+// if (tempreature<0) {
+//     console.log("cold weather");
+// }
+// else if (tempreature>0 && tempreature<20) {
+//     console.log("cool weather");
+    
+// }else {
+//     console.log("hot weather");
+    
+// }
+// let a=6
+// let b=9
+// let c=15
+// if (a>c && a>b) {
+//     console.log("a en boyuk eded");
+    
+// }
+// else if (b>a && b>c) {
+//     console.log("b en boyuk");
+    
+// }else {
+//     console.log("en boyuk c");
+    
+// }
+
+
+
+
+
+
+
+
+
